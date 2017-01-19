081f097a9a4672eaf101995c674bc9faa6a46405
Jordi <jordi@jormc.es>
2017-01-19 12:19:55 +0100

    Reoganized the router

--- server/app.js
+++ server/app.js
@@ -7,11 +7,6 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-/**
- * Route Imports
- */
-var signup = require('./routes/signup');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -29,39 +24,24 @@ if (app.get('env') === 'development') {
     // This covers serving up the index page
     app.use(express.static(path.join(__dirname, '../client/.tmp')));
     app.use(express.static(path.join(__dirname, '../client/app')));
-
-    // Error Handling
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
 }
 
 /**
  * Production Settings
  */
 if (app.get('env') === 'production') {
-
     // changes it to use the optimized version for production
     app.use(express.static(path.join(__dirname, '/dist')));
-
-    // production error handler
-    // no stacktraces leaked to user
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: {}
-        });
-    });
 }
 
 /**
  * Routes
  */
-app.use('/signup', signup);
+var router = require('./router')(app);
+
+// Error Handling
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+});
 
 module.exports = app;
